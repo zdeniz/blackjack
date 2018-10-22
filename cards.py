0a57bea31a520fe36cb4b94f0e367afd6c506038
zdeniz <z.denizhasirci95@gmail.com>
2018-10-22 16:03:54 +0300

    Kart, rank ve suit sabitleri class'dan modül içine alındı.
    Oyuncu sayısını alan metod yazıldı.
    
    Oyuncu ve dağıtıcının elindeki kartları tutan ve sayılarını oyun kurallarına göre hesaplayan classlar ve modülleri yazıldı.
    
    TODO listeleri eklendi.
    
    Bazı hatalar düzeltildi.

--- cards.py
+++ cards.py
@@ -1,24 +1,28 @@
 """
 This module for cards
+
+#TODO: Kartları sırayla dağıtan metod yazılacak.
+#TODO: 'Player Actions'lar için class ve/veya metod yazılacak.
 """
+
 import random
+
+card_ranks = {'Ace': 1, 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5,'Six': 6, 'Seven': 7,
+              'Eight': 8, 'Nine': 9, 'Ten': 10, 'Joker': 10, 'Queen': 10, 'King': 10}
+card_suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
 
 
 class Deck:
     """
     Deck class
     """
-    card_ranks = {'Ace': [1, 11], 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5,
-                  'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10,
-                  'Joker': 10, 'Queen': 10, 'King': 10}
-    card_suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
 
     def __init__(self, whole_deck=[]):
         self.whole_deck = whole_deck
 
-        list_of_ranks = list(Deck.card_ranks)
+        list_of_ranks = list(card_ranks)
         for i in range(4):
-            this_suit = Deck.card_suits[i]
+            this_suit = card_suits[i]
             for j in range(13):
                 self.whole_deck.append(this_suit + ' of ' + list_of_ranks[j])
 
@@ -32,7 +36,7 @@ class Deck:
         """
         random.shuffle(self.whole_deck)
 
-    def pull_from_dec(self):
+    def pull_from_deck(self):
         """
         Pull cards from deck
         :return: Pulled card name (str)
@@ -45,10 +49,6 @@ class Deck:
 
         return self.whole_deck.pop()
 
-    def put_cart_to_the_table(self, put_to_where):
-
-        return put_to_where.append(self.pull_from_dec())
-
 
 class SetTable:
     """
@@ -58,17 +58,69 @@ class SetTable:
         self.number_of_players = number_of_players
         while True:
             try:
-                self.number_of_players = int(input('Please Write Player Count'))
+                self.number_of_players = int(input('Please Write Player Count: '))
             except ValueError:
-                print('input must be integer')
+                print('Input must be integer!')
                 continue
             else:
-                print('Players Ready!')
-                break
+                if self.number_of_players > 6:
+                    print('Maximum player count is 6!')
+                    continue
+                elif self.number_of_players < 1:
+                    print('Minimum player count is 1!')
+                    continue
+                else:
+                    print('Players Ready!')
+                    break
 
-    def distribute_cards(self, deck):
-        for i in range(self.number_of_players):
-            deck.put_cart_to_the_table('player ' + str(i) + '\'s cards')
 
-        deck.put_cart_to_the_table('Dealer \'s cards')
+class Hands:
+    """
+    Player's cards
+    """
+    def __int__(self):
+        self.deck_in_the_hand = []
+
+    def __str__(self):
+        return self.deck_in_the_hand
+
+    def take_the_card(self, the_card):
+        self.deck_in_the_hand.append(the_card)
+
+    def calculate_the_hand(self):
+        """
+        Calculates the card ranks and add all up ace functions included
+        :return: value of the hand (int)
+        """
+        soft_hand = 0
+        rank_of_hand = 0
+        for i in self.deck_in_the_hand:
+            card_name = i.split(' ')
+            if card_name[-1] == 'Ace':
+                soft_hand += 1
+            else:
+                rank_of_hand += card_ranks[card_name[-1]]
+
+        if soft_hand >= 1:
+            if (21 - rank_of_hand - soft_hand + 1) >= 11:
+                rank_of_hand += (10 + soft_hand)
+            else:
+                rank_of_hand += soft_hand
+
+        return rank_of_hand
+
+
+class DealersHand(Hands):
+    """
+    Dealer's cards
+    """
+    def __init__(self, first_round=True):
+        Hands.__init__(self)
+        self.first_round = first_round
+
+    def __str__(self):
+        if self.first_round:
+            return self.deck_in_the_hand[0] + '\nClosed card! '
+        else:
+            return self.deck_in_the_hand
 
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from cards import Deck, SetTable
+"""
+File Name: Main.py
+Author: zdeniz
+Year: 2018
+#TODO: Oyun arayüzü için metod ve/veya class oluşturulacak.
+#TODO: Oyuncu ve dağıtıcı bahis ve para hesapları yapan ve bu değerleri tutan class ve/veya metodlar oluşturulacak.
+"""
+import cards
 
-deck = Deck()
-
+deck = cards.Deck()
 
 print(deck)
 deck.shuffle_deck()
 print(deck)
-print(deck.card_ranks['King'])
-bb = SetTable()
-bb.distribute_cards(deck)
+print(cards.card_ranks['King'])
+bb = cards.SetTable()
 print(deck)
 
-print('fkeljnfdsbjfbds')
+abc = cards.DealersHand(True)
